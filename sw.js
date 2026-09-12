6c92931474563b7e10cce6545fcceea85ba3b065
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-12 14:07:54 +0900

    Cache v0.6 access terminal

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='download-tycoon-v5';
-const ASSETS=['./','./index.html','./style.css','./game.js','./file-list.js','./connection-patch.js','./manifest.json','./icon.svg'];
+const CACHE='download-tycoon-v6';
+const ASSETS=['./','./index.html','./style.css','./game.js','./file-list.js','./connection-patch.js','./cheat.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
